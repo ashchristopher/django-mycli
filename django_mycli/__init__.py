8e467c31ffd08c2786b087b8f0a97dba6891117e
Maciej Dziardziel <m.dziardziel@cv-library.co.uk>
2019-02-15 11:19:21 +0000

    make it work with recent django

--- django_mycli/__init__.py
+++ django_mycli/__init__.py
@@ -4,13 +4,24 @@ __author__ = 'Ash Christopher'
 __email__ = 'ash.christopher@gmail.com'
 __version__ = '0.0.1'
 
+import subprocess
 
 from django.db.backends.mysql import base
 from django.db.backends.mysql.client import DatabaseClient
+from django.db import connections
 
 
 class mycliDatabaseClient(DatabaseClient):
     executable_name = 'mycli'
 
-base.__old_database_client = base.DatabaseClient
+    def runshell(self):
+        args = self.settings_to_cmd_args(self.connection.settings_dict)
+        subprocess.check_call(args)
+
+
 base.DatabaseClient = mycliDatabaseClient
+base.client_class = mycliDatabaseClient
+
+for conn in connections.all():
+    if isinstance(conn.client, DatabaseClient):
+        conn.client = mycliDatabaseClient(conn)
