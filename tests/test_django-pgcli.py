e423ce44763f0c77fb016da30c99002ed86c0b7d
Ash Christopher <ash.christopher@shopify.com>
2019-07-20 23:39:07 -0400

    Fixes tests

--- tests/test_django-pgcli.py
+++ tests/test_django-pgcli.py
@@ -10,18 +10,13 @@ class RuntimeTestCase(unittest.TestCase):
     def test_database_client_subclassed(self):
         from django.db.backends.mysql import base
 
-        self.assertEqual(base.DatabaseClient, django_mycli.mycliDatabaseClient)
+        self.assertEqual(base.DatabaseWrapper.client_class, django_mycli.MyCLIDatabaseClient)
 
     def test_database_client_calls_mycli_executable(self):
         from django.db.backends.mysql import base
-          
-        self.assertEqual(base.DatabaseClient.executable_name, 'mycli')
 
-    def test_old_database_client_set_on_base(self):
-        from django.db.backends.mysql import base
-        from django.db.backends.mysql.client import DatabaseClient
-        
-        self.assertEqual(getattr(base, '__old_database_client'), DatabaseClient)
+        self.assertEqual(base.DatabaseWrapper.client_class.executable_name, 'mycli')
+
 
 if __name__ == '__main__':
     unittest.main()
